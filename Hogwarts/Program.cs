7c9e187c9dc643e74c98774d03ed6baf92b01df6
student <student@17-03>
2019-07-15 21:13:10 +0300

    atractions

--- Hogwarts/Program.cs
+++ Hogwarts/Program.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Hogwarts.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Hogwarts
@@ -14,7 +17,29 @@ namespace Hogwarts
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                HogwartsContext context = services.GetRequiredService<HogwartsContext>();
+
+                if (context.Atractions.Count() == 0)
+                {
+                    context.Add(new Atractions()
+                    {
+                        Name = "לונה הוגסמיד",
+                        Kind = 1,
+                        Address = "טבריה",
+
+
+
+                    });
+                    context.SaveChanges();
+                }
+
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
